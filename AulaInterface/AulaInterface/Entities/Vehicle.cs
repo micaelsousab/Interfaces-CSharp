133988507f43f25fcd4497f860a9725c7ce27ec9
Micael Sousa <micaelsousabrb@gmail.com>
2021-01-13 21:23:42 -0300

    primeira parte

---
+++ AulaInterface/AulaInterface/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace AulaInterface.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ AulaInterface/AulaInterface/Program.cs
@@ -0,0 +1,23 @@
+﻿using AulaInterface.Entities;
+using System;
+using System.Globalization;
+
+namespace AulaInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter rental data: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Pickup (dd/mm/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Return (dd/mm/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+        }
+    }
+}
